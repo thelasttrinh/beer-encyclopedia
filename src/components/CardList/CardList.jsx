bf68c393fd3e386572ffe841aa0f3f6e23375aa8
thelasttrinh <aarontrinh57@yahoo.com>
2023-04-07 00:36:36 -0500

    Cards displaying correctly, will add tooltip for extra info

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -2,16 +2,30 @@ import React from 'react'
 //@ts-ignore
 import Cards from '../Cards/Cards';
 import "../../App.scss";
+import {useState} from 'react'
 
 const CardList = (props) => {
-    const {beers} = props;
+  const {beers} = props;
+  const [showInfo, setShowInfo] = useState(false);
+
+  const handleTruncate = (description) => {
+    if(description.length > 50){
+      return `${description.substring(0,50)}...`;
+    }
+    return description;
+  };
+
+  const handleToolTip = () =>{
+    setShowInfo(!showInfo);
+  };
+
   return (
     
     //Use map for display cards
     <div>
         {beers.map((beer) => 
         <div className='beers__main__CardList'>
-            <Cards beers={beer}/>
+            <Cards beers={beer} handleTruncate={handleTruncate} handleToolTip={handleToolTip}/>
         </div>)}
     </div>
   )
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 
 const Cards = (props) => {
-  const {beers} = props;
+  const {beers, handleTruncate, handleToolTip} = props;
+
   return (
     <div className='beers__main__CardList_Cards'>
       <img src={beers.image_url} alt={beers.tagline}/>
+      <a href="#" onClick={handleToolTip}>{beers.name}</a>
+      <p >{handleTruncate(beers.description)}</p>
     </div>
   )
 }
